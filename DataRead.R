fc4de140e704b71df1bf35d4481d7a3a7c7e5c37
turdlez <ciadams@iastate.edu>
2016-10-10 10:07:41 -0500

    DataRead

---
+++ DataRead.R
@@ -0,0 +1,11 @@
+#Reading data in
+#Read your CSV in
+Ponds <- read.csv("Data01.csv")
+head(Ponds)
+str(Ponds)
+summary(Ponds)
+
+#To-do List
+#Get rid of negative numbers and replace them with 0
+#Change to LongForm
+#Log your readings and create that CSV file
